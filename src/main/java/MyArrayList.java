9efb13c1ad9ec07af0b487078144e41a8ef73c3e
Eduard <e9047689558@yandex.ru>
2022-07-28 09:05:13 +0300

    create interface

--- src/main/java/MyArrayList.java
+++ src/main/java/MyArrayList.java
@@ -1,4 +1,12 @@
-public interface MyArrayList
+public interface MyArrayList <T>
 {
-    
+    boolean add(T t);
+    boolean add(int index);
+    T get(int index);
+    T get(T t);
+    void set(int index, T t);
+    void remove(int index);
+    void remove(T t);
+    int size();
+    void clear();
 }
